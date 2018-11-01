02a4a22dd20c1c7e66b87b0402b0264d46364c82
SmajevicIrfan <smajevic.irfan@gmail.com>
2018-11-01 10:13:09 +0100

    Created class Imenik

---
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Imenik {
+    private Map<String, TelefonskiBroj> phonebook;
+
+    public Imenik() {
+        phonebook = new HashMap<>();
+    }
+    
+}
