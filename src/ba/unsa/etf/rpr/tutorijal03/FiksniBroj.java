80f3c08149afa98375594dc01107ff162341a1a5
SmajevicIrfan <smajevic.irfan@gmail.com>
2018-11-01 09:38:27 +0100

    Created class FiksniBroj

---
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -0,0 +1,40 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class FiksniBroj extends TelefonskiBroj {
+    public enum Grad {
+        TRAVNIK("030"),
+        ORASJE("031"),
+        ZENICA("032"),
+        SARAJEVO("033"),
+        LIVNO("034"),
+        TUZLA("035"),
+        MOSTAR("036"),
+        BIHAC("037"),
+        GORAZDE("038"),
+        SIROKIBRIJEG("039"),
+        BRCKO("049");
+
+        private final String callNumber;
+
+        public Grad (String pozivniBroj) {
+            this.callNumber = pozivniBroj;
+        }
+
+        public String getPozivniBroj() {
+            return this.callNumber;
+        }
+    }
+
+    private Grad city;
+    private String number;
+
+    public FiksniBroj(Grad grad, String broj) {
+        this.city = grad;
+        this.number = broj;
+    }
+
+    @Override
+    public String ispisi() {
+        return String.format("%s/%s", this.city.getPozivniBroj(), this.number);
+    }
+}
